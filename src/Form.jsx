5d577d073eb5cafff0b8f8c016e69bb5b00d7fe8
ArDezZz <aromudez@gmail.com>
2022-08-11 15:48:04 +0300

    some changes

--- src/Form.jsx
+++ src/Form.jsx
@@ -13,32 +13,12 @@ const Form = ({ showForm }) => (
     <h1>Registration</h1>
     <Formik
       initialValues={{ email: '', password: '' }}
-      // validate={values => {
-      //   const errors = {};
-      //   if (!values.email) {
-      //     errors.email = 'Required';
-      //   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      //     errors.email = 'Invalid email address';
-      //   }
-      //   return errors;
-      // }}
       onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
+        alert(JSON.stringify(values, null, 2));
+        setSubmitting(false);
       }}
     >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
+      {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
         <form className="form-menu" onSubmit={handleSubmit}>
           <ThemeProvider theme={themeGreen}>
             <TextField
